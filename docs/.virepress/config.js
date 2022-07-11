643c7dda13dc9b7b9d6575fe2dc05ce513655057
coveychen95 <coveychen95@outlook.com>
2022-07-11 23:30:31 +0800

    feat: 1

--- docs/.virepress/config.js
+++ docs/.virepress/config.js
@@ -1,4 +1,17 @@
 module.exports = {
   title: '陈可为',
-  description: '为人类进步而努力！'
+  description: '为人类进步而努力！',
+  base: '/blog/',
+  themeConfig: {
+    lastUpdated: '最后更新时间',
+    sidebar: 'auto',
+    repo: 'https://github.com/coveychen95/blog',
+    repoLabel: 'Github',
+    nav: [
+      {
+        text: 'test',
+        link: '/01JavaScript/'
+      },
+    ]
+  }
 }
